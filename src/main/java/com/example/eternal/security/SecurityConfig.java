aa472a148f3f6ea15f2370d18c601898a796dfda
westjuuu <westjiuuu@naver.com>
2024-09-08 21:12:44 +0900

    fefactor: config 분리

--- src/main/java/com/example/eternal/security/SecurityConfig.java
+++ src/main/java/com/example/eternal/security/SecurityConfig.java
@@ -12,8 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -36,27 +35,19 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, UrlBasedCorsConfigurationSource corsConfigurationSource) throws Exception {
         http
-                .cors(cors -> cors.configurationSource(request -> {
-                    CorsConfiguration config = new CorsConfiguration();
-                    config.addAllowedOrigin("https://sswu-eternal.com");  // 클라이언트 도메인 허용
-                    config.addAllowedOrigin("http://localhost:3000");  // 로컬 개발 허용
-                    config.addAllowedMethod("*");  // 모든 HTTP 메서드 허용
-                    config.addAllowedHeader("*");  // 모든 헤더 허용
-                    config.setAllowCredentials(true);  // 자격 증명 허용 (쿠키나 인증 헤더 허용)
-                    return config;
-                }))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))  // CORS 설정 사용
                 .csrf(AbstractHttpConfigurer::disable)  // CSRF 비활성화
                 .exceptionHandling(exceptionHandling ->
                         exceptionHandling.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // 세션 관리 정책 설정
                 .authorizeHttpRequests(authorize ->
                         authorize
-                                .requestMatchers("/user/send-verification-code", "/user/verify-email", "/user/register", "/user/login", "/test", "/manager/login", "/notices/**").permitAll()
-                                .requestMatchers("/user/stamp/**", "/profile").authenticated()
-                                .anyRequest().authenticated());
+                                .requestMatchers("/user/send-verification-code", "/user/verify-email", "/user/register", "/user/login", "/test", "/manager/login", "/notices/**").permitAll()  // 인증 없이 접근 가능한 경로
+                                .requestMatchers("/user/stamp/**", "/profile").authenticated()  // 인증이 필요한 경로
+                                .anyRequest().authenticated());  // 그 외의 모든 요청은 인증이 필요
 
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);  // JWT 필터 추가
 
